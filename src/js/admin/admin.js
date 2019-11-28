0b8ed677ef9ff2b5e132bf1d9a16ae6b338bb623
GraceWalk <gracewalk1999@163.com>
2019-11-28 09:14:07 +0800

    完成上传模块

--- src/js/admin/admin.js
+++ src/js/admin/admin.js
@@ -1,13 +1,10 @@
-console.log('加载成功')
-AV.init({
-    appId: "rdhStfmRmkhd82NsDe6xE6fe-gzGzoHsz",
-    appKey: "44JqP2vHHlw7KDLe6bzirBFg",
-    serverURLs: "https://rdhstfmr.lc-cn-n1-shared.com"
-});
-
 // var TestObject = AV.Object.extend('TestObject');
 // var testObject = new TestObject();
 // testObject.set('words', 'Hello world!');
 // testObject.save().then(function(testObject) {
 //     console.log('保存成功。')
 // })
+
+window.eventHub.on('uploaded', (data) => {
+    console.log(data);
+})
---
+++ src/js/av.js
@@ -0,0 +1,5 @@
+AV.init({
+    appId: "rdhStfmRmkhd82NsDe6xE6fe-gzGzoHsz",
+    appKey: "44JqP2vHHlw7KDLe6bzirBFg",
+    serverURLs: "https://rdhstfmr.lc-cn-n1-shared.com"
+});
