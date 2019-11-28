5c9bf2361f5d5a18cf2325c18985c5b3bee171a8
GraceWalk <gracewalk1999@163.com>
2019-11-28 16:30:20 +0800

    添加管理页面的增、改、查功能

--- src/js/admin/new-song.js
+++ src/js/admin/new-song.js
@@ -2,7 +2,7 @@
     let view = {
         el: '#new-song-box',
         template: `
-            <div class="back">
+            <div class="back" id="back">
                 <svg t="1574907301736" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2485" width="24" height="24"><path d="M528.527 1024c-16.05 0-32.099-5.98-44.345-17.981L24.469 555.5c-24.492-23.962-24.492-62.874 0-86.876l459.713-450.56c24.492-24.003 64.157-24.003 88.649 0 24.492 23.962 24.492 62.874 0 86.876L157.463 512.041l415.368 407.06c24.492 24.003 24.492 62.874 0 86.876-12.204 12.042-28.254 18.022-44.304 18.022z" p-id="2486" fill="#535970"></path></svg>
             </div>
             <form class="new-song">
@@ -22,19 +22,27 @@
                     </div>
                 </div>
                 <div class="change">
-                    <div class="cancel">取消</div>
+                    <div class="cancel" id="cancel">取消</div>
                     <input type="submit" class="save" value="保存">
                 </div>
             </form>
         `,
-        render(data = {}) {
+        init() {
+            this.$el = $(this.el);
+        },
+        render(data = {}) { //将data渲染到input框中
             let placeholders = ['name', 'singer', 'url'];
             let html = this.template;
             placeholders.map((string) => {
                 html = html.replace(`__${string}__`, data[string] || '');
             });
-            $(this.el).html(html);
-            $(this.el).hide();
+            this.$el.html(html);
+        },
+        show() { //展示页面
+            this.$el.show()
+        },
+        hide() { //隐藏页面
+            this.$el.hide()
         }
     }
 
@@ -42,49 +50,93 @@
         data: {
             name: '',
             singer: '',
-            url: ''
+            url: '',
+            id: ''
         },
-        getData() {
+        saveData() {
             let str = ['name', 'singer', 'url'];
             str.map((string) => {
                 this.data[string] = $(`input[name=${string}]`).val();
             });
         },
-        createSong() {
+        createSong() { //新建歌曲上传到LeanCloud
             let Songs = AV.Object.extend('Songs');
+
             let newSong = new Songs();
             newSong.set('name', this.data.name);
             newSong.set('singer', this.data.singer);
             newSong.set('url', this.data.url);
-            newSong.save().then((data) => {
-                console.log('创建成功');
-                console.log(data);
-            })
+
+            return newSong.save();
+        },
+        updateSong() {
+            let song = AV.Object.createWithoutData('Songs', this.data.id);
+
+            song.set('name', this.data.name);
+            song.set('singer', this.data.singer);
+            song.set('url', this.data.url);
+
+            return song.save();
         }
     }
 
     let controller = {
-        init(view, model) {
+        init(view, model) { //初始化
             this.view = view;
+            this.view.init();
+            this.view.hide();
             this.model = model;
             this.view.render();
             this.bindEventHubs();
             this.bindEvents();
         },
-        bindEvents() {
-            $(this.view.el).on('submit', 'form', (e) => {
+        bindEvents() { //绑定事件
+            this.view.$el.on('submit', 'form', (e) => {
                 e.preventDefault();
-                this.model.getData();
-                this.model.createSong();
+                this.model.saveData();
+                this.view.render();
+                if (this.model.data.id === '') {
+                    console.log('创建歌曲')
+                    this.model.createSong().then(() => {
+                        window.eventHub.emit('switchPage');
+                    });
+                } else {
+                    console.log('更新歌曲')
+                    this.model.updateSong().then(() => {
+                        window.eventHub.emit('switchPage');
+                    });
+                }
+            });
+            this.view.$el.on('click', '#back', () => {
+                window.eventHub.emit('switchPage');
+            });
+            this.view.$el.on('click', '#cancel', () => {
+                window.eventHub.emit('switchPage');
+                this.view.render();
             })
         },
-        bindEventHubs() {
-            window.eventHub.on('uploaded', (data) => {
+        bindEventHubs() { //绑定订阅事件
+            window.eventHub.on('uploaded', (data) => { //上传歌曲完成
                 let songName = data.substr(0, data.length - 4);
                 let url = `http://q1lwd4mpb.bkt.clouddn.com/${encodeURIComponent(songName)}.mp3`;
                 this.model.data.name = songName;
                 this.model.data.url = url;
+                this.view.show();
                 this.view.render(this.model.data);
+            });
+            window.eventHub.on('editSong', (data) => { //编辑歌曲
+                this.view.show();
+                this.model.data = data;
+                this.view.render(this.model.data);
+            });
+            window.eventHub.on('switchPage', () => { //切换页面
+                if (this.view.$el.is(':hidden')) {
+                    console.log('展示新建歌曲页面')
+                    this.view.show();
+                } else {
+                    console.log('隐藏新建歌曲页面')
+                    this.view.hide();
+                }
             })
         }
     }
